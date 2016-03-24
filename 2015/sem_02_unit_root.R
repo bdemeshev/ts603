97585207e8bdf9632938a161067b8b999b3e0ea2
Boris Demeshev <boris.demeshev@gmail.com>
2016-03-24 21:14:08 +0300

    arma + unit

---
+++ 2015/sem_02_unit_root.R
@@ -0,0 +1,24 @@
+
+x <- arima.sim(n = 200,
+        model = list(ar = 0.6))
+y <- arima.sim(n = 200,
+        model = list(ma = c(0.7, 0.2)))
+library("forecast")
+tsdisplay(x)
+tsdisplay(y)
+
+model <- lm(y ~ x)
+summary(model)
+
+x2 <- cumsum(x)
+y2 <- cumsum(y)
+
+tsdisplay(x2)
+tsdisplay(y2)
+
+model <- lm(y2 ~ x2)
+summary(model)
+
+library(tseries)
+adf.test(y2)
+
